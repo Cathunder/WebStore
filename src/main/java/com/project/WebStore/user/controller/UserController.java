d82105a29f61d55193bcbda94d2586607288c24c
takealook <macaroonv@gmail.com>
2024-06-25 17:53:42 +0900

    feat: 유저 회원가입 api 추가

--- src/main/java/com/project/WebStore/user/controller/UserController.java
+++ src/main/java/com/project/WebStore/user/controller/UserController.java
@@ -1,8 +1,26 @@
 package com.project.WebStore.user.controller;
 
+import com.project.WebStore.user.dto.RegisterUserDto;
+import com.project.WebStore.user.dto.UserDto;
+import com.project.WebStore.user.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
 public class UserController {
 
+  private final UserService userService;
+
+  @PostMapping("/register")
+  public ResponseEntity<?> register(@RequestBody @Valid RegisterUserDto.Request request) {
+    UserDto userDto = userService.register(request);
+    return ResponseEntity.ok(RegisterUserDto.Response.fromDto(userDto));
+  }
 }
--- src/main/java/com/project/WebStore/user/service/UserService.java
+++ src/main/java/com/project/WebStore/user/service/UserService.java
@@ -1,10 +1,35 @@
 package com.project.WebStore.user.service;
 
+import com.project.WebStore.user.dto.RegisterUserDto;
+import com.project.WebStore.user.dto.UserDto;
+import com.project.WebStore.user.entity.UserEntity;
+import com.project.WebStore.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
+  private final UserRepository userRepository;
+  private final PasswordEncoder passwordEncoder;
+
+  @Transactional
+  public UserDto register(RegisterUserDto.Request request) {
+    if (userRepository.existsByEmail(request.getEmail())) {
+      throw new RuntimeException("동일한 email이 존재합니다.");
+    }
+
+    UserEntity userEntity = UserEntity.builder()
+        .email(request.getEmail())
+        .password(passwordEncoder.encode(request.getPassword()))
+        .nickname(request.getNickname())
+        .point(1000)
+        .cash(0)
+        .build();
+
+    return UserDto.fromEntity(userRepository.save(userEntity));
+  }
 }
