7407613d0439511864d4fb8cbdd9778b5af5bed3
takealook <macaroonv@gmail.com>
2024-06-26 21:37:28 +0900

    refactor: 중복되는 빈을 공통설정파일로 이동

--- src/main/java/com/project/WebStore/security/SecurityConfiguration.java
+++ src/main/java/com/project/WebStore/security/SecurityConfiguration.java
@@ -7,8 +7,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -33,10 +31,5 @@ public class SecurityConfiguration {
     http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
     return http.build();
-  }
-
-  @Bean
-  public PasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
   }
 }
