7e06cd7f95cb7e2ff6dcacd799f8d26d11b670ac
takealook <macaroonv@gmail.com>
2024-07-03 17:00:12 +0900

    feat: 불필요한 코드 삭제

--- src/main/java/com/project/WebStore/error/GlobalExceptionHandler.java
+++ src/main/java/com/project/WebStore/error/GlobalExceptionHandler.java
@@ -3,16 +3,12 @@ package com.project.WebStore.error;
 import static com.project.WebStore.error.ErrorCode.INTERNAL_SERVER_ERROR;
 
 import com.project.WebStore.error.exception.WebStoreException;
-import jakarta.validation.ConstraintViolationException;
 import java.util.List;
-import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
